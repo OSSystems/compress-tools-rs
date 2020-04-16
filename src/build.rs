5eb4927a13c343f1981804baf0aa757200595f3d
asakiz <asakizin@gmail.com>
2020-04-16 09:45:52 -0300

    Remove outdated comment
    Signed-off-by: asakiz <asakizin@gmail.com>

--- src/build.rs
+++ src/build.rs
@@ -1,6 +1,4 @@
 fn main() {
-    // This forces the tests to run in a single thread. This is
-    // required for use of the mocks as we run mocked binaries.
     pkg_config::Config::new()
         .atleast_version("3.2.2")
         .probe("libarchive")
