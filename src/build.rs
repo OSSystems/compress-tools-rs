c6d1c3182fb8eefcc6992d48c036b92acf843e62
Jonathas-Conceicao <jonathas.conceicao@ossystems.com.br>
2020-04-29 11:22:25 -0300

    Add support for windows build through vcpkg
    Signed-off-by: Jonathas-Conceicao <jonathas.conceicao@ossystems.com.br>

--- src/build.rs
+++ src/build.rs
@@ -1,6 +1,18 @@
 fn main() {
+    if !find_libarchive() {
+        panic!("Unable to find libarchive");
+    }
+}
+
+#[cfg(not(windows))]
+fn find_libarchive() -> bool {
     pkg_config::Config::new()
         .atleast_version("3.2.2")
         .probe("libarchive")
-        .expect("Fail to detect the libarchive library");
+        .is_ok()
+}
+
+#[cfg(windows)]
+fn find_libarchive() -> bool {
+    vcpkg::Config::new().find_package("libarchive").is_ok()
 }
