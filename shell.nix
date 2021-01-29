f048c6a9a4ea69180f0976a5b2bee6ec56067b0f
Otavio Salvador <otavio@ossystems.com.br>
2021-01-29 08:41:03 -0300

    general: Add a lorri and direnv support to easy development on NixOS
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+stdenv.mkDerivation {
+  name = "compress-tools";
+  buildInputs = [
+    pkg-config
+    libarchive
+    clang
+    llvmPackages.libclang
+  ];
+  # why do we need to set the library path manually?
+  shellHook = ''
+      export LIBCLANG_PATH="${llvmPackages.libclang}/lib";
+  '';
+}
