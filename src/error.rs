de1d7e2012c24f889310ec3914abc25c57edc151
Otavio Salvador <otavio@ossystems.com.br>
2021-05-29 10:05:36 -0300

    Avoid impl of custom Display and use inner one
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- src/error.rs
+++ src/error.rs
@@ -14,14 +14,11 @@ pub enum Error {
     #[display(fmt = "Extraction error: '{}'", _0)]
     Extraction(#[error(not(source))] String),
 
-    #[display(fmt = "{}", _0)]
     Io(io::Error),
 
-    #[display(fmt = "{}", _0)]
     Utf(std::str::Utf8Error),
 
     #[cfg(feature = "tokio_support")]
-    #[display(fmt = "{}", _0)]
     JoinError(tokio::task::JoinError),
 
     #[display(fmt = "Error to create the archive struct, is null")]
