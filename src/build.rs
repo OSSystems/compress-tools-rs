8c477291def1e046a87d313f5c29e6b8c2e5645d
Iain Laird <ilaird@expediagroup.com>
2021-09-09 16:43:02 +0100

    Add static Windows support
    Emit additional link values to environment in build.rs.
    Run static build check and test.
    Differentiate original Windows build check and test by adding keyword "dynamic" to names.

--- src/build.rs
+++ src/build.rs
@@ -19,4 +19,8 @@ fn find_libarchive() {
     vcpkg::Config::new()
         .find_package("libarchive")
         .expect("Unable to find libarchive");
+
+    println!("cargo:rustc-link-lib=static=archive");
+    println!("cargo:rustc-link-lib=User32");
+    println!("cargo:rustc-link-lib=Crypt32");
 }
