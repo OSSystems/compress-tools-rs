e01c61ac3aa843a77ac5d3af0afc2654e77e8fc8
Otavio Salvador <otavio@ossystems.com.br>
2022-03-19 11:03:43 -0300

    nix: Move from shell.nix to flake.nix
    This reduces the risk of changes in dependencies and used a pinned set
    of libraries and tools.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "compress-tools-rs";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-21.11";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rust-bindgen
+            pkg-config
+            libarchive
+            clang
+            llvmPackages.libclang
+          ];
+
+          # why do we need to set the library path manually?
+          shellHook = ''
+            export LIBCLANG_PATH="${pkgs.llvmPackages.libclang}/lib";
+          '';
+        };
+      });
+}
