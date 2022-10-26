48b64602be01910d5e3d3603f44dfebe5acf74e1
Otavio Salvador <otavio@ossystems.com.br>
2022-10-26 13:55:01 -0300

    flake: use MSRV for development
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- flake.nix
+++ flake.nix
@@ -7,16 +7,38 @@
       url = "github:numtide/flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    rust = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        rust-toolchain = with rust.packages.${system};
+          let
+            channel_1_59_0 = {
+              channel = "1.59.0";
+              sha256 = "sha256-4IUZZWXHBBxcwRuQm9ekOwzc0oNqH/9NkI1ejW7KajU=";
+            };
+
+            stable_1_59_0 = toolchainOf channel_1_59_0;
+          in
+          combine [
+            (stable_1_59_0.withComponents [ "rustc" "cargo" "rust-src" "clippy" ])
+
+            latest.rustfmt
+            latest.rust-analyzer
+          ];
       in
       {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
+            rust-toolchain
             rust-bindgen
             pkg-config
             libarchive
