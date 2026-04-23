f2fe3308dc562dd3739d6ccf837f010867370801
Otavio Salvador <otavio@ossystems.com.br>
2026-04-23 11:17:54 -0300

    chore: migrate flake to red-tape and bump nixpkgs to 25.11
    - Replace the flake-utils boilerplate with red-tape's mkFlake, moving the devShell definition
      into a dedicated devshell.nix that red-tape picks up automatically.
    - Bump nixpkgs to nixos-25.11 and refresh the fenix / rust-analyzer lock entries that come with
      it.
    - Drop the stale shellHook comment now that the surrounding code has been tidied up.

--- flake.nix
+++ flake.nix
@@ -2,8 +2,11 @@
   description = "compress-tools-rs";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-25.11";
+    red-tape = {
+      url = "github:phaer/red-tape";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     rust = {
       url = "github:nix-community/fenix";
@@ -11,47 +14,8 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        rust-toolchain = with rust.packages.${system};
-          let
-            msrvToolchain = toolchainOf {
-              channel = "1.82.0";
-              sha256 = "sha256-yMuSb5eQPO/bHv+Bcf/US8LVMbf/G/0MSfiPwBhiPpk=";
-            };
-          in
-          combine [
-            (msrvToolchain.withComponents [ "rustc" "cargo" "rust-src" "clippy" ])
-
-            latest.rustfmt
-            latest.rust-analyzer
-          ];
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            rust-toolchain
-            rust-bindgen
-            pkg-config
-            libarchive
-            # Required by the `static` feature (build.rs probes these via
-            # pkg-config when libarchive is linked statically).
-            libb2
-            lz4
-            zstd
-            clang
-            llvmPackages.libclang
-
-            cargo-release
-          ];
-
-          # why do we need to set the library path manually?
-          shellHook = ''
-            export LIBCLANG_PATH="${pkgs.llvmPackages.libclang}/lib";
-          '';
-        };
-      });
+  outputs = inputs: inputs.red-tape.mkFlake {
+    inherit inputs;
+    src = ./.;
+  };
 }
