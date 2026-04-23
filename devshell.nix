5488ceaffb1d6ff7e9204141a2af5a277d6c24cb
Otavio Salvador <otavio@ossystems.com.br>
2026-04-23 11:17:54 -0300

    chore: migrate flake to red-tape and bump nixpkgs to 25.11
    - Replace the flake-utils boilerplate with red-tape's mkFlake, moving the devShell definition
      into a dedicated devshell.nix that red-tape picks up automatically.
    - Bump nixpkgs to nixos-25.11 and refresh the fenix / rust-analyzer lock entries that come with
      it.
    - Drop the stale shellHook comment now that the surrounding code has been tidied up.

---
+++ devshell.nix
@@ -0,0 +1,38 @@
+{ pkgs, lib, inputs, system, ... }:
+
+let
+  rust-toolchain = with inputs.rust.packages.${system};
+    let
+      msrvToolchain = toolchainOf {
+        channel = (lib.importTOML ./Cargo.toml).package.rust-version;
+        sha256 = "sha256-yMuSb5eQPO/bHv+Bcf/US8LVMbf/G/0MSfiPwBhiPpk=";
+      };
+    in
+    combine [
+      (msrvToolchain.withComponents [ "rustc" "cargo" "rust-src" "clippy" ])
+
+      latest.rustfmt
+      latest.rust-analyzer
+    ];
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    rust-toolchain
+    rust-bindgen
+    pkg-config
+    libarchive
+    # Required by the `static` feature (build.rs probes these via
+    # pkg-config when libarchive is linked statically).
+    libb2
+    lz4
+    zstd
+    clang
+    llvmPackages.libclang
+
+    cargo-release
+  ];
+
+  shellHook = ''
+    export LIBCLANG_PATH="${pkgs.llvmPackages.libclang}/lib";
+  '';
+}
